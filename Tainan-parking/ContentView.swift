44112c9889d3e4a18b065e1a1fd330c641525322
Ateto1202 <tonysu1204@gmail.com>
2024-04-12 07:30:55 +0800

    Initial Commit

---
+++ Tainan-parking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tainan-parking
+//
+//  Created by 蘇東毅 on 2024/4/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tainan-parking/Tainan_parkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tainan_parkingApp.swift
+//  Tainan-parking
+//
+//  Created by 蘇東毅 on 2024/4/12.
+//
+
+import SwiftUI
+
+@main
+struct Tainan_parkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
