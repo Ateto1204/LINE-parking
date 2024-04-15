d8dd3617782ee0ba0314f1b81ab30c24ff2904c3
Ateto1202 <tonysu1204@gmail.com>
2024-04-15 13:40:07 +0800

    Clean up source

--- LINE-parking/View/ParkingListView/ParkingDataView.swift
+++ LINE-parking/View/ParkingListView/ParkingDataView.swift
@@ -34,55 +34,7 @@ struct ParkingDataView: View {
                 Spacer()
             }
             if animating {
-                VStack {
-                    HStack {
-                        VStack(alignment: .leading, spacing: 16) {
-                            HStack {
-                                ShareLink("", item: appStatus.shareInfo(spot: appStatus.displaySpot.spot))
-                                Text(appStatus.displaySpot.spot.name)
-                                    .font(.title)
-                                    .bold()
-                                Button {
-                                    withAnimation {
-                                        appStatus.displaySpot.liked.toggle()
-                                    }
-                                    appStatus.toggleLoveSpot(spot: appStatus.displaySpot)
-                                } label: {
-                                    if appStatus.isLiked(spot: appStatus.displaySpot) {
-                                        Image(systemName: "heart.fill")
-                                            .foregroundColor(.red)
-                                            .opacity(1)
-                                            .transition(
-                                                .movingParts.pop(.red)
-                                            )
-                                    } else {
-                                        Image(systemName: "heart")
-                                            .foregroundColor(.red)
-                                            .opacity(0.5)
-                                            .transition(.identity)
-                                    }
-                                }
-                                .padding(.leading)
-                            }
-                            Text("地址：\(appStatus.displaySpot.spot.address)")
-                            
-                            Text("大型車：\(appStatus.displaySpot.spot.largeCar)")
-                            Text("小型車：\(appStatus.displaySpot.spot.smallCar)")
-                            Text("無障礙：\(appStatus.displaySpot.spot.disableCar)")
-                            Text("機慢車：\(appStatus.displaySpot.spot.motorcycle)")
-                        }
-                        .padding()
-                        Spacer()
-                    }
-                    Button {
-                        let urlBrowser = URL(string: "https://google.com/maps/search/\(appStatus.displaySpot.spot.address)")
-                        UIApplication.shared.open(urlBrowser!, options: [:], completionHandler: nil)
-                    } label: {
-                        Text("開始導航")
-                            .shadow(radius: 0.74)
-                    }
-                    .padding(.top)
-                }
+                ParkingDataContentView()
                 .padding()
                 .transition(
                   .asymmetric(
